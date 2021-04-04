4908fd8150b2fb4bc472c224f75be0248bfb8074
js <js201910271836@outlook.com>
2021-04-05 01:32:50 +0200

    mise-à-jour: 05-04-2021 1:32:49

--- push.bat
+++ push.bat
@@ -1,7 +1,9 @@
 @echo off
 chcp 65001
-set a=0;
-set date_auj=%date:~0,2%-%date:~3,2%-20%date:~6,4%
+set a=1;
+set d=%date:~0,2%-%date:~3,2%-20%date:~6,4%
+set t=%time:~0,2%:%time:~3,2%:%time:~6,2%
+set t=%t: =%
 if not exist ".git" ( git init )
 if not exist "README.md" ( echo "# C_SDL" >> README.md )
 if %a%==0 (
@@ -11,12 +13,12 @@ if %a%==0 (
     git branch -M main
     git pull https://github.com/js202005082300/C_SDL.git
     git add *
-    git commit -a -m "mise-à-jour: %date_auj%"
+    git commit -a -m "mise-à-jour: %d% %t%"
     git push -u origin main
 ) else ( 
     git pull https://github.com/js202005082300/C_SDL.git
     git add *
-    git commit -a -m "mise-à-jour: %date_auj%"
+    git commit -a -m "mise-à-jour: %d% %t%"
     git push https://github.com/js202005082300/C_SDL.git
 )
 exit
