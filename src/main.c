9380540a088e014c8b530fb7b98f654e97386fb6
js <js201910271836@outlook.com>
2021-04-13 02:15:55 +0200

    13-04-2021 2:15:54

--- src/main.c
+++ src/main.c
@@ -5,21 +5,24 @@
 
 #include "utils.h"
 
-int main(int argc, char **argv)
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+int main(int argc, char *argv[])
 {
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
     SDL_Surface *background = NULL;
     SDL_Texture *texture = NULL;
-    SDL_Rect dest_rect = {0, 0, 640, 480};
+    SDL_Rect dstrect = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
 
     if(SDL_Init(SDL_INIT_VIDEO)!=0)
         SDL_ExitWithError("Initialisation SDL echouee");
-
-    window = SDL_CreateWindow("Jeu C/SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    
+    window = SDL_CreateWindow("Jeu C/SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     if(window == NULL)
         SDL_ExitWithError("Creation fenetre echouee");
-
+    
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
     if(renderer == NULL)
     {
@@ -42,22 +45,23 @@ int main(int argc, char **argv)
         SDL_ExitWithError("Creation texture echouee");
     }
 
-    if(SDL_QueryTexture(texture, NULL, NULL, &dest_rect.w, &dest_rect.h) != 0)
+    if(SDL_QueryTexture(texture, NULL, NULL, &dstrect.w, &dstrect.h) != 0)
     {
-        SDL_Log("ERREUR > %s\n", SDL_GetError());
         SDL_CleanRessources(window, renderer, texture);
-        exit(EXIT_FAILURE);
+        SDL_ExitWithError("Impossible de charger la texture");
     }
 
-    if(SDL_RenderCopy(renderer, texture, NULL, &dest_rect) != 0)
+    // dstrect.x = (WINDOW_WIDTH - dstrect.w) / 2;
+    // dstrect.y = (WINDOW_HEIGHT - dstrect.h) / 2;
+
+    if(SDL_RenderCopy(renderer, texture, NULL, &dstrect) != 0)
     {
-        SDL_Log("ERREUR > %s\n", SDL_GetError());
         SDL_CleanRessources(window, renderer, texture);
-        exit(EXIT_FAILURE);
+        SDL_ExitWithError("Impossible d'afficher la texture");
     }
 
     SDL_RenderPresent(renderer);
-    SDL_Delay(5000);
+    SDL_Delay(3000);
 
     SDL_CleanRessources(window, renderer, texture);
     return EXIT_SUCCESS;
